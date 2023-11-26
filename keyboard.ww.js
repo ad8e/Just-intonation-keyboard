636dc6f99373758ae7e9b5aa81721d3cc33e477e
- <19739817+ad8e@users.noreply.github.com>
2023-11-26 13:13:53 -0800

    automated update

--- keyboard.ww.js
+++ keyboard.ww.js
@@ -1,1 +1,1 @@
-onmessage=function(d){onmessage=null;d=d.data;self.Module=d;d["instantiateWasm"]=(info,receiveInstance)=>{var instance=new WebAssembly.Instance(d["wasm"],info);receiveInstance(instance,d["wasm"]);return instance.exports};importScripts(d.js);d.wasm=d.mem=d.js=0};
+"use strict";onmessage=function(d){onmessage=null;d=d.data;self.Module=d;d["instantiateWasm"]=(info,receiveInstance)=>{var instance=new WebAssembly.Instance(d["wasm"],info);return receiveInstance(instance,d["wasm"])};importScripts(d.js);d.wasm=d.mem=d.js=0};
